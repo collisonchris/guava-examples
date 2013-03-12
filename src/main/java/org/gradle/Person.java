1e2ea0eab507cf77ddd1288b598cd77358051c17
Chris Collison <collisonchris@gmail.com>
2013-03-12 17:48:38 -0500

    adding more unit tests and examples for Person

--- src/main/java/org/gradle/Person.java
+++ src/main/java/org/gradle/Person.java
@@ -2,8 +2,9 @@ package org.gradle;
 
 import com.google.common.base.Objects;
 import com.google.common.base.Preconditions;
+import com.google.common.collect.ComparisonChain;
 
-public class Person {
+public class Person implements Comparable<Person> {
     private String name;
     private String shortName;
     private Integer age;
@@ -13,6 +14,7 @@ public class Person {
         this.name = name;
         this.age = age;
         this.isTall = isTall;
+        this.shortName=shortName;
     }
 
     public String getName() {
@@ -53,6 +55,7 @@ public class Person {
             Person that = (Person) object;
             return Objects.equal(this.name, that.name)
                     && Objects.equal(this.age, that.age)
+                    && Objects.equal(this.shortName, that.shortName)
                     && Objects.equal(this.isTall, that.isTall);
         }
         return false;
@@ -65,8 +68,12 @@ public class Person {
 
     @Override
     public String toString() {
-        return Objects.toStringHelper(this).add("name", name).add("age", age)
-                .add("isTall", isTall).toString();
+        return Objects.toStringHelper(this)
+                .add("name", name)
+                .add("age", age)
+                .add("isTall", isTall)
+                .add("shortName", shortName)
+                .toString();
     }
 
     public String preferredName() {
@@ -77,5 +84,22 @@ public class Person {
         Preconditions.checkState(age != null,
                 "Can't have a null age and drink!");
         return age >= 21;
+    }
+
+    public int oldCompareTo(Person that) {
+        if(this.age > that.age || (this.age == that.age && this.isTall && !that.isTall)) {
+            return 1;
+        } else if( this.age < that.age || (this.age == that.age && !this.isTall && that.isTall)) {
+            return -1;
+        }
+        return 0;
+
+    }
+    
+    public int compareTo(Person that) {
+        return ComparisonChain.start()
+                .compare(this.age, that.age)
+                .compare(this.isTall, that.isTall)
+                .result();
     }
 }
--- src/test/java/org/gradle/PersonTest.java
+++ src/test/java/org/gradle/PersonTest.java
@@ -7,16 +7,20 @@ import static org.junit.Assert.*;
 public class PersonTest {
     Person chris;
     Person tom;
+    Person jerry;
+    Person chrisCyborg;
 
     @Before
     public void before() {
         chris = new Person("Christopher", 23, Boolean.FALSE, "Chris");
-        tom = new Person("Thomas", 19, Boolean.TRUE, null);
+        chrisCyborg = new Person("Christopher", 23, Boolean.FALSE, "Chris");
+        tom = new Person("Thomas", 19, Boolean.TRUE, "Tom");
+        jerry = new Person("Jerry", 23, Boolean.TRUE, "J");
+        
     }
 
     @Test
     public void getPreferredNameShort() {
-        tom.setShortName("Tom");
         assertEquals("Tom", tom.preferredName());
     }
 
@@ -37,6 +41,26 @@ public class PersonTest {
         chris.setAge(null);
 
         chris.canDrink();
+    }
+    
+    @Test
+    public void printPerson() {
+        assertEquals("Person{name=Christopher, age=23, isTall=false, shortName=Chris}", chris.toString());
+    }
+    
+    @Test
+    public void comparatorTest() {
+        //both work functionally the same way
+        assertEquals(1,chris.oldCompareTo(tom));
+        assertEquals(-1,chris.oldCompareTo(jerry));
+        
+        assertEquals(1,chris.compareTo(tom));
+        assertEquals(-1,chris.compareTo(jerry));
+        
+        assertEquals(0,chris.compareTo(chrisCyborg));
+        assertEquals(0,chris.compareTo(chrisCyborg));
+        
+        
     }
 
 }
